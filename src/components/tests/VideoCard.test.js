223b9ae6eeaa923758b50471c892fbd2b05a4cd7
5hyun <qhslsl@gmail.com>
2023-06-21 04:02:23 +0900

    Feat: 테스트 코드 리팩토링

--- src/components/tests/VideoCard.test.js
+++ src/components/tests/VideoCard.test.js
@@ -3,32 +3,16 @@ import VideoCard from "../VideoCard";
 import { MemoryRouter, Route, Routes, useLocation } from "react-router-dom";
 import { format } from "timeago.js";
 import userEvent from "@testing-library/user-event";
+import { fakeVideo as video } from "../../tests/videos";
+import { withRouter } from "../../tests/utils";
 
 describe("VideoCard", () => {
-  const video = {
-    id: 1,
-    snippet: {
-      title: "title",
-      channelId: "1",
-      channelTitle: "channelTitle",
-      publishedAt: new Date(),
-      thumbnails: {
-        medium: {
-          url: "http://image/",
-        },
-      },
-    },
-  };
-
   const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
 
   it("renders video item", () => {
     // render를 통해 테스트하고자 하는 컴포넌트를 불러온다
     render(
-      //  VideoCard 컴포넌트에서 Router가 사용되고 있어서 MemoryRouter를 사용한다
-      <MemoryRouter>
-        <VideoCard video={video} />
-      </MemoryRouter>
+      withRouter(<Route path={"/"} element={<VideoCard video={video} />} />)
     );
     //  렌더링을 시키고 확인을 해보면된다.
 
@@ -53,18 +37,16 @@ describe("VideoCard", () => {
     }
 
     render(
-      //  initialEntries는 처음에 시작하는 경로
-      <MemoryRouter initialEntries={["/"]}>
-        {/*네비게이션 역할 때문에 Routes 사용*/}
-        <Routes>
+      withRouter(
+        <>
           <Route path={"/"} element={<VideoCard video={video} />} />
           {/* 클릭했을때 이동 할 라우터 */}
           <Route
             path={`/videos/watch/${video.id}`}
             element={<LocationStateDisplay />}
           />
-        </Routes>
-      </MemoryRouter>
+        </>
+      )
     );
 
     // listitem은 li를 의미한다.
