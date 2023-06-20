494cb3816df192ea879500d5f0476cb163431dec
5hyun <qhslsl@gmail.com>
2023-06-21 04:37:07 +0900

    Feat: 스냅샷 테스트

--- src/components/tests/VideoCard.test.js
+++ src/components/tests/VideoCard.test.js
@@ -1,14 +1,33 @@
 import { render, screen } from "@testing-library/react";
 import VideoCard from "../VideoCard";
-import { MemoryRouter, Route, Routes, useLocation } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
 import { format } from "timeago.js";
 import userEvent from "@testing-library/user-event";
 import { fakeVideo as video } from "../../tests/videos";
 import { withRouter } from "../../tests/utils";
+import renderer from "react-test-renderer";
 
 describe("VideoCard", () => {
   const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
 
+  // 스냅샷 테스트
+  it("renders gird type correctly", () => {
+    const component = renderer.create(
+      withRouter(<Route path={"/"} element={<VideoCard video={video} />} />)
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it("renders list type correctly", () => {
+    const component = renderer.create(
+      withRouter(
+        <Route path={"/"} element={<VideoCard video={video} type={"list"} />} />
+      )
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  // 정적 테스트
   it("renders video item", () => {
     // render를 통해 테스트하고자 하는 컴포넌트를 불러온다
     render(
@@ -29,6 +48,7 @@ describe("VideoCard", () => {
     expect(screen.getByText(format(publishedAt))).toBeInTheDocument();
   });
 
+  // 동적 테스트
   it("navigates to detailed video page with video state when clicked", () => {
     // Location의 상태를 표현할 수 있는 컴포넌트(테스트용 함수 컴포넌트), 즉 네비게이션을 통해 이동할 때 같이 따라 온 데이터를 임의의 보여주트
     function LocationStateDisplay() {
