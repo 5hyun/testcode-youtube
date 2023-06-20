c6d7672ec51ff74b1d03967e680a8a32df1a5e38
5hyun <qhslsl@gmail.com>
2023-06-21 03:31:28 +0900

    Feat: 동적 테스트

--- src/components/tests/VideoCard.test.js
+++ src/components/tests/VideoCard.test.js
@@ -1,7 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import VideoCard from "../VideoCard";
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, Route, Routes, useLocation } from "react-router-dom";
 import { format } from "timeago.js";
+import userEvent from "@testing-library/user-event";
 
 describe("VideoCard", () => {
   const video = {
@@ -42,5 +43,36 @@ describe("VideoCard", () => {
     expect(screen.getByText(title)).toBeInTheDocument();
     expect(screen.getByText(channelTitle)).toBeInTheDocument();
     expect(screen.getByText(format(publishedAt))).toBeInTheDocument();
+  });
+
+  it("navigates to detailed video page with video state when clicked", () => {
+    // Location의 상태를 표현할 수 있는 컴포넌트(테스트용 함수 컴포넌트), 즉 네비게이션을 통해 이동할 때 같이 따라 온 데이터를 임의의 보여주트
+    function LocationStateDisplay() {
+      //  Router에서 받아온 state를 보여준다.
+      return <pre>{JSON.stringify(useLocation().state)}</pre>;
+    }
+
+    render(
+      //  initialEntries는 처음에 시작하는 경로
+      <MemoryRouter initialEntries={["/"]}>
+        {/*네비게이션 역할 때문에 Routes 사용*/}
+        <Routes>
+          <Route path={"/"} element={<VideoCard video={video} />} />
+          {/* 클릭했을때 이동 할 라우터 */}
+          <Route
+            path={`/videos/watch/${video.id}`}
+            element={<LocationStateDisplay />}
+          />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    // listitem은 li를 의미한다.
+    const card = screen.getByRole("listitem");
+    //  클릭 이벤트를 발생시킨다.
+    userEvent.click(card);
+
+    //  클릭이되면 페이지 경로가 바뀐다. 따라서 video가 화면 안애 있는지 확인하면 된다.
+    expect(screen.getByText(JSON.stringify({ video }))).toBeInTheDocument();
   });
 });
