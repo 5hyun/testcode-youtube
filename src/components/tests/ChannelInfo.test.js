a1a40088417de3e4591fd88abab8c4e946cadb4d
5hyun <qhslsl@gmail.com>
2023-06-23 02:34:14 +0900

    Feat: 채널 컴포넌트 테스트

--- src/components/tests/ChannelInfo.test.js
+++ src/components/tests/ChannelInfo.test.js
@@ -16,6 +16,47 @@ describe("ChannelInfo", () => {
     // fakeYoutube에 channelImageURL이 호출되면 mockImplementation을 설정해서 url을 리턴하도록 만든다.
     fakeYoutube.channelImageURL.mockImplementation(() => "url");
 
+    // 이미지는 통신으로 받아오는거라서 render를 먼저한다. render를 하면 asFragment를 반환해준다.
+    const { asFragment } = render(
+      withAllContexts(
+        withRouter(
+          <Route
+            path={"/"}
+            element={<ChannelInfo id={"id"} name={"channel"} />}
+          />
+        ),
+        fakeYoutube
+      )
+    );
+
+    //  react query는 비동기라서 waitFor을 사용해서 기다려줘야한다. 즉, 이미지나 타나날 때 까지 기다림
+    await waitFor(() => screen.getByRole("img"));
+    // 반환받은 fragment를 통해 스냅샷을 만든다.
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  //  url을 가지고 오지 못한 경우
+  it("renders without URL", () => {
+    fakeYoutube.channelImageURL.mockImplementation(() => {
+      throw new Error("error");
+    });
+    render(
+      withAllContexts(
+        withRouter(
+          <Route path={"/"} element={<ChannelInfo id="id" name="channel" />} />
+        ),
+        fakeYoutube
+      )
+    );
+
+    //
+    expect(screen.queryByRole("img")).toBeNull();
+  });
+
+  //  URL이 있으면 이미지가 반드시 보여야 한다는 테스트
+  // 이 부분은 스냅샷 테스트를 하기 때문에 굳이 안해도 되지만 테스트를 좀 더 명시적으로 하고싶을때 하면 된다.
+  it("renders with URL", async () => {
+    fakeYoutube.channelImageURL.mockImplementation(() => "url");
     render(
       withAllContexts(
         withRouter(
@@ -28,7 +69,6 @@ describe("ChannelInfo", () => {
       )
     );
 
-    //  react query는 비동기라서 waitFor을 사용해서 기다려줘야한다.
-    await waitFor(() => screen.getByText("channel"));
+    await waitFor(() => expect(screen.getByRole("img")).toBeInTheDocument());
   });
 });
